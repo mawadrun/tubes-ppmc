cf952adc33022bb07028e7614d039f275ed3eda2
mawadrun <mawadrun@gmail.com>
2024-05-13 21:04:21 +0700

    notes

--- main.c
+++ main.c
@@ -64,6 +64,21 @@ int aStar(char matrix[MAX_COL][MAX_ROW], int m, int n)
 
     printf("Start: %d, %d\nEnd: %d, %d\n", start.x, start.y, end.x, end.y);
     printf("Distance: %d\n", distance);
+
+    /*
+    - Buat dua buah list, open dan closed.
+      Open list digunakan untuk menyimpan kandidat titik yang dapat dikunjungi,
+      dan closed list digunakan untuk menyimpan titik yang sudah dikunjungi.
+    - Masukkan titik awal ke closed list. Jadikan ia sebagai titik aktif
+    - Masukkan keempat tetangga dari titik aktif yang mungkin
+      dikunjungi; yakni arah bawah, kanan, atas, dan kiri; ke dalam open list.
+    - Selama open list tidak kosong:
+      - Dapatkan koordinat dari open list yang memiliki cost terkecil. Jadikan
+        sebagai titik aktif.
+      - Hilangkan titik aktif dari open list. Tambahkan ke closed list
+      - Periksa keempat tetangga dari titik aktif yang mungkin dikunjungi dan
+        tidak ada di closed list, tambahkan keempatnya ke open list.
+    */
 }
 
 int main()
