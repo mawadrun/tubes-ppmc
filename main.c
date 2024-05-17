ac607ccf1fa1fe58a2ff87c620ed422044b5a758
mawadrun <mawadrun@gmail.com>
2024-05-17 22:55:15 +0700

    init main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "astar.c"
+#include "backtracking.c"
+#include "dfs.c"
+#include "dijkstra.c"
+#include "divide&conquer.c"
+#include "labirinBFS.c"
+
+int main()
+{
+    printf("Hello");
+
+    return 0;
+}
