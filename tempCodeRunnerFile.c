d436d79c5552745d039fa0bbac6f116ebe675901
mawadrun <mawadrun@gmail.com>
2024-05-17 11:04:22 +0700

    deleted everything in gitignore

--- tempCodeRunnerFile.c
+++
@@ -1,9 +0,0 @@
-        // printf("\n");
-        // printf("Priority Queue: ");
-        // struct PriorityQueueNode *print = head;
-        // while (print != NULL)
-        // {
-        //     printf("(%d, %d, %d, %d) ", (print->coords).x, (print->coords).y, (print->coords).g, (print->coords).h);
-        //     print = print->next;
-        // }
-        // printf("\n");
