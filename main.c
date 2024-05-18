464b3d1157edf6bd4aac3bc81346377edba5f875
azra haikal fahlevi <azrahaikal75@gmail.com>
2024-05-18 15:10:07 +0700

    udah dibikin menu di main, tapi ga bisa jalan :(

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <time.h>
 
 #include "astar.c"
+#include "Greedy.c"
 #include "backtracking.c"
 #include "dfs.c"
 #include "dijkstra.c"
@@ -20,7 +21,7 @@
 
 int main()
 {
-    int pilihanAlgorithm;
+
     bacaFile();             
     // tes print isi file txt
     printf("\nBentuk maze-nya:\n");
@@ -29,7 +30,39 @@ int main()
     }
     printf("\nJumlah baris = %d\nJumlah kolom = %d", baris, kolom);
 
-    printf("variabel m dari divide: m = %d", baris);
+    int pilihanAlgorithm;
+    printf("\nMau pake algoritma apaan??\n");
+    printf("1. greedy\n");
+    printf("2. A*\n");
+    printf("3. backtracking\n");  
+    printf("4. DFS\n");  
+    printf("5. dijkstra\n");  
+    printf("6. divide and conquer\n");    
+    printf("7. BFS\n");
+    printf("> ");
+    scanf("%d", pilihanAlgorithm);
+
+    if(pilihanAlgorithm == 1){
+        main_greedy();
+    }
+    else if(pilihanAlgorithm == 2){
+        main_astar();
+    }
+    else if(pilihanAlgorithm == 3){
+        main_back();
+    }
+    else if(pilihanAlgorithm == 4){
+        main_dfs();
+    }
+    else if(pilihanAlgorithm == 5){
+        main_dijkstra();
+    }
+    else if(pilihanAlgorithm == 6){
+        main_divide();
+    }
+    else if(pilihanAlgorithm == 7){
+        main_bfs();
+    }
 
     return 0;
 }
